31f52211d4d081e65c3ef175e5e94ab774cb6f13
Odumosu Dipo <dodumosu@timbaobjects.com>
2021-04-08 23:16:35 +0100

    feat: add utlities

--- apollo/pwa/static/src/utils.js
+++ apollo/pwa/static/src/utils.js
@@ -15,4 +15,31 @@ const formSorter = (a, b) => {
     return a.id - b.id;
 };
 
-export { formSorter };
+const computeGroupCompletion = (form, submission, groupIndex) => {
+  let group = form.data.groups[groupIndex];
+
+  let completion = {total: group.fields.length};
+  completion.filled = group.fields.reduce((acc, field) => {
+    if (field.type === 'image')
+      acc += (submission.images[field.tag] !== undefined ? 1 : 0);
+    else if (field.type === 'multiselect')
+      acc += (submission.data[field.tag].length > 0 ? 1: 0);
+    else
+      acc += (submission.data[field.tag] !== undefined ? 1 : 0);
+
+    return acc;
+  }, 0);
+
+  return completion;
+};
+
+const formCompletion = (form, submission) => form.data.groups.map((_, index) => computeGroupCompletion(form, submission, index));
+
+const formCompletionAsObject = (form, submission) => formCompletion(form, submission).reduce((acc, completion) => {
+  acc[filled] += completion.filled;
+  acc[total] += completion.total;
+
+  return acc;
+}, {total: 0, filled: 0});
+
+export { computeGroupCompletion, formSorter, formCompletion, formCompletionAsObject };

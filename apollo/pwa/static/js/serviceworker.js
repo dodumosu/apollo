f2de869536157fa5590f5d0af74a4a49e2445381
Odumosu Dipo <dodumosu@timbaobjects.com>
2021-04-08 16:43:36 +0100

    chore: change indentation

--- apollo/pwa/static/js/serviceworker.js
+++ apollo/pwa/static/js/serviceworker.js
@@ -1,63 +1,63 @@
 const CACHE_NAME = 'apollo-cache-static-svelte-v1';
 
 const CACHED_URLS = [
-    '/pwa/',
-    '/pwa/static/css/main.css',
-    '/pwa/static/img/icons-48.png',
-    '/pwa/static/img/icons-72.png',
-    '/pwa/static/img/icons-96.png',
-    '/pwa/static/img/icons-192.png',
-    '/pwa/static/js/app.js',
-    '/pwa/static/public/build/bundle.css',
-    '/pwa/static/public/build/bundle.js',
-    '/pwa/static/vendor/bootstrap/css/bootstrap.min.css',
-    '/pwa/static/vendor/bootstrap/css/bootstrap.rtl.min.css',
-    '/pwa/static/vendor/bootstrap/js/bootstrap.min.js',
+  '/pwa/',
+  '/pwa/static/css/main.css',
+  '/pwa/static/img/icons-48.png',
+  '/pwa/static/img/icons-72.png',
+  '/pwa/static/img/icons-96.png',
+  '/pwa/static/img/icons-192.png',
+  '/pwa/static/js/app.js',
+  '/pwa/static/public/build/bundle.css',
+  '/pwa/static/public/build/bundle.js',
+  '/pwa/static/vendor/bootstrap/css/bootstrap.min.css',
+  '/pwa/static/vendor/bootstrap/css/bootstrap.rtl.min.css',
+  '/pwa/static/vendor/bootstrap/js/bootstrap.min.js',
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(CACHED_URLS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(CACHED_URLS)));
 });
 
 self.addEventListener('fetch', (event) => {
-    const requestURL = new URL(event.request.url);
-    if (requestURL.pathname === '/pwa/') {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(
-                cache => cache.match('/pwa/').then(
-                    cacheResponse => {
-                        let fetchPromise = fetch('/pwa/').then(
-                            networkResponse => {
-                                cache.put('/pwa/', networkResponse.clone());
-                                return networkResponse;
-                            }
-                        );
-                        return cacheResponse || fetchPromise;
-                    }
-                )
-            )
+  const requestURL = new URL(event.request.url);
+  if (requestURL.pathname === '/pwa/') {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(
+        cache => cache.match('/pwa/').then(
+          cacheResponse => {
+            let fetchPromise = fetch('/pwa/').then(
+              networkResponse => {
+                cache.put('/pwa/', networkResponse.clone());
+                return networkResponse;
+              }
+            );
+            return cacheResponse || fetchPromise;
+          }
         )
-    } else if (CACHED_URLS.includes(requestURL.href) || CACHED_URLS.includes(requestURL.pathname)) {
-        event.respondWith(
-            caches.open(CACHE_NAME).then(
-                cache => cache.match(event.request)
-                    .then(response => response || fetch(event.request))
-            )
-        );
-    }
+      )
+    )
+  } else if (CACHED_URLS.includes(requestURL.href) || CACHED_URLS.includes(requestURL.pathname)) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(
+        cache => cache.match(event.request)
+          .then(response => response || fetch(event.request))
+      )
+    );
+  }
 });
 
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then(
-            (cacheNames) => Promise.all(cacheNames.map((cacheName) => {
-                if (CACHE_NAME !== cacheName && cacheName.startsWith('apollo-cache-static')) {
-                    return caches.delete(cacheName);
-                }
-            }))
-        )
-    );
+  event.waitUntil(
+    caches.keys().then(
+      (cacheNames) => Promise.all(cacheNames.map((cacheName) => {
+        if (CACHE_NAME !== cacheName && cacheName.startsWith('apollo-cache-static')) {
+          return caches.delete(cacheName);
+        }
+      }))
+    )
+  );
 });
